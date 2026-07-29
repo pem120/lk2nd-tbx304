msm8916-mainline/lk2nd

Add Lenovo Tab 4 10(TB-X304x) support

lk2nd/device/dts/msm8952/rules.mk
@@ -2,6 +2,7 @@
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
 ADTBS += \
+	$(LOCAL_DIR)/msm8917-lenovo-tbx304x.dtb \
 	$(LOCAL_DIR)/msm8917-mtp.dtb \
 	$(LOCAL_DIR)/msm8917-xiaomi-common.dtb \
 	$(LOCAL_DIR)/msm8917-xiaomi-riva.dtb \

pem120/lk2nd-tbx304

lk2nd/device/dts/msm8952/rules.mk
@@ -3,6 +3,7 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 
 ADTBS += \
 	$(LOCAL_DIR)/msm8917-mtp.dtb \
+	$(LOCAL_DIR)/msm8917-qrd-sku5.dtb \
 	$(LOCAL_DIR)/msm8917-xiaomi-common.dtb \
 	$(LOCAL_DIR)/msm8917-xiaomi-riva.dtb \
 	$(LOCAL_DIR)/msm8920-motorola-jeter.dtb \